668ef86352b38f1e1f290d3c1ec68571ace6d1f7
Damir <who.ami.2021@mail.ru>
2023-09-13 11:40:20 +0600

    addstyles popup

--- src/components/ProductFlats/Flats.jsx
+++ src/components/ProductFlats/Flats.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Flats.css";
 
 export const Flats = ({ items }) => {
   return (
--- src/components/ProductFlats/ProductFlats.jsx
+++ src/components/ProductFlats/ProductFlats.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Flats } from "./Flats";
+import "./ProductFlats.css";
 
 export const ProductFlats = ({ flats }) => {
   return (
-    <div>
+    <div className="productflats">
       {flats.map((item) => {
         return <Flats items={item} key={item.LOT} />;
       })}
